4cfc95b23fcac03c274660f5b2744b0f737b456f
Joevishah Mergil <joevishah2000@gmail.com>
2026-03-01 10:16:24 +0530

    Updated the API_URL

--- frontend/src/app/api.ts
+++ frontend/src/app/api.ts
@@ -1,4 +1,5 @@
-export const API_URL = 'http://localhost:8000/api';
+// export const API_URL = 'http://localhost:8000/api';
+export const API_URL = (typeof (import.meta as any).env !== 'undefined' && (import.meta as any).env && (import.meta as any).env.VITE_API_URL) || 'http://localhost:8000/api';
 
 export const getAuthHeaders = () => {
     const token = localStorage.getItem('token');
